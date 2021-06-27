af3b0a0193edfd3af25b68e09013c874ad9bdc56
Neil Guan <guan840912@gmail.com>
2021-06-27 21:34:50 +0800

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,7 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'ecs-Bottlerocket-cp',
+});
+project.synth();
